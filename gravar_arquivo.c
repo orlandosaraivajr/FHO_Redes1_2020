6550c44a989f168e1ddaace7b3f670af9b5238a8
Orlando Saraiva Jr <orlandosaraivajr@gmail.com>
2020-10-29 20:45:57 -0300

    Exemplo de como gravar no arquivo

---
+++ gravar_arquivo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+   FILE * pFile;
+   int n;
+   char name [100];
+
+   pFile = fopen ("arquivo.txt","w");
+   for (n=0 ; n<3 ; n++)
+   {
+     puts ("Digite seu nome: ");
+     gets (name);
+     fprintf (pFile, "Nome %d [%-10.10s]\n",n+1,name);
+   }
+   fclose (pFile);
+
+   return 0;
+}
